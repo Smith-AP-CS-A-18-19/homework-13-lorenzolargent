64159d1fa87e42dfcdf87c1d02202600eac3864f
Lorenzo Largent <largelor@berkeleyprep.org>
2019-02-11 11:15:10 -0500

    Homework 13 Done

--- Homework13.java
+++ Homework13.java
@@ -14,21 +14,22 @@ public class Homework13 {
 	  * and angle. The x and y coordinates should both be 0.
 	  * Return the Triangle.
 	  */
-	 public static Triangle problem1(int w, int h, double a) {f
-
+	 public static Triangle problem1(int w, int h, double a) {
+Triangle t = new Triangle(0,0,w, h, a);
+return t;
 	 }
 
 	 /* Return the area of parameter s
 	  */
 	 public static double problem2(SmithShape s) {
-
+return s.getArea();
 	 }
 
 	 /* Return the value returned by the toString()
 	  * method of parameter s
 	  */
 	 public static String problem3(SmithShape s) {
-
+return s.toString();
 	 }
 
 	 /* Given SmithShape s, which is a Triangle,
@@ -38,14 +39,16 @@ public class Homework13 {
 	  * to 180
 	  */
 	 public static double problem4(SmithShape s, double a) {
-
+double t = 180;
+t -=((Triangle) (s)).getAngle()+a;
+return t;
 	 }
 
 	 /* Return the Rectangle2D that is created by the
 	  * toSquare method of parameter s
 	  */
 	 public static Rectangle2D problem5(SmithShape s) {
-
+return ((Triangle) (s)).toSquare();
 	 }
 
 	 public static void main(String[] args) {
--- Triangle.java
+++ Triangle.java
@@ -3,25 +3,65 @@ import java.awt.geom.Rectangle2D;
  * the horizontal and are defined by the angle (in degrees)
  * of the lower left corner.
  */
-
+public class Triangle implements SmithShape{
+  private int x;
+  private int y;
+  private int w;
+  private int h;
+  private double a;
 /* The constructor for the Triangle object should be as below.
  * You may change the names of the parameters, but their order
  * must remain the same for the assignment to be scored
  * correctly
  */
+public Triangle(int x1, int y1, int w1, int h1, double a1){
+  x=x1;
+  y=y1;
+  w=w1;
+  h=h1;
+  a=a1;
+}
+
 // public Triangle(int x, int y, int width, int height, double angle)
 
 /* You will need to add a getAngle() method that returns
  * the angle of the triangle
  */
+public int getX(){
+  return x;
+}
 
+public int getY(){
+  return y;
+}
+
+public int getWidth(){
+  return w;
+}
+
+public int getHeight(){
+  return h;
+}
+
+public double getArea(){
+  return w*h/2;
+}
+
+public double getAngle(){
+  return a;
+}
 /* Add the method toSquare() to the Triangle class. This creates
  * and returns a new Rectangle2D object. The x and y of the new
  * Rectangle should be the same as the Triangle. The width and
  * height of the Rectangle2D should be the same (it is a square)
  * and the area should be equal to the area of the Triangle
  */
-
+public Rectangle2D toSquare(){
+  Rectangle2D r1 = new Rectangle2D.Double();
+  double l2 = Math.sqrt(getArea());
+  r1.setRect(x,y,l2,l2);
+  return r1;
+}
 /* Override the method toString(). It should return
  * the String:
  * Width: width
@@ -30,3 +70,9 @@ import java.awt.geom.Rectangle2D;
  * Remember that to insert a new line into a String,
  * use \n
  */
+ @Override
+ public String toString(){
+   String xx = "Width: " + w + "\nHeight: " + h + "\nAngle: " + a;
+   return xx;
+ }
+}
